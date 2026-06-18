0a3068e45f255289190e2b574a1b86afa829dc28
Abhishek Kolge <abhishekkolge96@gmail.com>
2026-06-18 21:19:18 +0530

    first commit

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -3,6 +3,6 @@ import { env } from "@uni-gpt/env/web";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: env.NEXT_PUBLIC_SERVER_URL,
-  plugins: [polarClient()],
+	baseURL: env.NEXT_PUBLIC_SERVER_URL,
+	plugins: [polarClient()],
 });
